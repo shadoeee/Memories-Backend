0db611445f120093adb313f1c756d0234f6ab252
shadoeee <balaganesh541@gmail.com>
2024-05-01 01:16:16 +0530

    h

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to my MERN project!');
+});
+
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://balaganesh541:123123123@cluster0.gkrmpai.mongodb.net/chat?retryWrites=true&w=majority&ssl=true';
+const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
